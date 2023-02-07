140af56180497befd13779e12f194c031b7a3666
issam <issammez44@gmail.com>
2023-02-07 17:13:23 +0100

    add welcome page

---
+++ resources/views/components/btn.blade.php
@@ -0,0 +1,1 @@
+<button {{$attributes->merge(['class' => 'text-white focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 hover:bg-gray-700 focus:ring-gray-700 border-gray-700'])}}>{{$slot}}</button>
