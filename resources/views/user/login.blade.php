05c903ea2eabe93c8446ba89e7720109d898b7a9
issam <issammez44@gmail.com>
2023-02-12 15:03:55 +0100

    change the UI of register page

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -10,14 +10,14 @@
                         @csrf
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="">
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" value="{{old('email')}}" required="">
                             @error('email')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                             @enderror
                         </div>
                         <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="">
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" value="{{old('password')}}" required="">
                             @error('password')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                             @enderror
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,40 +1,56 @@
 <x-layout>
-    <div class="w-full h-screen flex justify-center items-center flex-col">
-        <div class="w-2/4 text-center rounded bg-[#eee] shadow-lg p-10">
-            <h1 class="uppercase text-center font-bold text-2xl mb-4">register</h1>
-            <form method="POST" action="/users">
-                @csrf
-                <div class="mb-6">
-                    <label for="username" class="float-left mb-2 font-semibold">Username</label>
-                    <input type="text" id="username" name="username" class="border text-sm rounded-lg block w-full p-2.5 bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="username" value="{{old('username')}}" required>
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                    @enderror
+    <section class="bg-gray-50">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                        Create an account
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" method="POST" action="/users">
+                        @csrf
+                        <div>
+                            <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
+                            <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Username" value="{{old('username')}}" required="">
+                            @error('username')
+                                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                            @enderror
+                        </div>
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" value="{{old('email')}}" required="">
+                            @error('email')
+                                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                            @enderror
+                        </div>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" value="{{old('password')}}" required="">
+                            @error('password')
+                                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                            @enderror
+                        </div>
+                        <div>
+                            <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
+                            <input type="password" name="password_confirmation" id="password_confirmation" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" value="{{old('password_confirmation')}}" required="">
+                            @error('password_confirmation')
+                                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                            @enderror
+                        </div>
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                              <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="">
+                            </div>
+                            <div class="ml-3 text-sm">
+                              <label for="terms" class="font-light text-gray-500">I accept the <a class="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
+                            </div>
+                        </div>
+                        <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                        <p class="text-sm font-light text-gray-500">
+                            Already have an account? <a href="/login" class="font-medium text-primary-600 hover:underline">Login here</a>
+                        </p>
+                    </form>
                 </div>
-                <div class="mb-6">
-                    <label for="email" class="float-left mb-2 font-semibold">Email</label>
-                    <input type="email" id="email" name="email" class="border text-sm rounded-lg block w-full p-2.5 bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="exemple@gmail.com" value="{{old('email')}}" required>
-                    @error('email')
-                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <label for="password" class="float-left mb-2 font-semibold">Password</label>
-                    <input type="password" id="password" name="password" class="border text-sm rounded-lg block w-full p-2.5 bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="••••••••" value="{{old('password')}}" required>
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <label for="password_confirmation" class="float-left mb-2 font-semibold">Repeat password</label>
-                    <input type="password" id="password_confirmation" name="password_confirmation" class="border text-sm rounded-lg block w-full p-2.5 bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="••••••••" value="{{old('password_confirmation')}}" required>
-                    @error('password_confirmation')
-                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                    @enderror
-                </div>
-                <x-btn>Register</x-btn>
-                <p>Already have an account? <a href="/login" class="font-bold">Login</a></p>
-            </form>            
+            </div>
         </div>
-    </div>
+    </section>
 </x-layout>
