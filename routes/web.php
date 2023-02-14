a12e4292a0095f9ad6a21c9d2eb9491db90021ca
issam <issammez44@gmail.com>
2023-02-14 15:08:09 +0100

    add middleware group

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MealController;
 use App\Http\Controllers\UserController;
@@ -21,113 +22,121 @@ use Illuminate\Support\Str;
 |
 */
 
-// index
-Route::get('/', [MealController::class, 'index']);
+Route::middleware('guest')->group(function() {
+    // index
+    Route::get('/', [MealController::class, 'index']);
+
+    // Show Login Form
+    Route::get('/login', [UserController::class, 'login'])->name('login');
+
+    // Show Register Form
+    Route::get('/register', [UserController::class, 'register']);
+
+    // Submit Register Form
+    Route::post('/users', [UserController::class, 'store']);
+
+    // Submit Login Form
+    Route::post('/auth', [UserController::class, 'authenticate']);
+
+    // The Password Reset Link Request Form
+    Route::get('/forgot-password', function () {
+        return view('user.forgot-password');
+    })->name('password.request');
+
+    // Handling The Form Submission
+    Route::post('/forgot-password', function (Request $request) {
+        $request->validate(['email' => 'required|email']);
+    
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+    
+        return $status === Password::RESET_LINK_SENT
+                    ? back()->with(['success_msg' => __($status)])
+                    : back()->with(['failed_msg' => __($status)]);
+    })->name('password.email');
+
+    // The Password Reset Form
+    Route::get('/reset-password/{token}', function ($token) {
+        return view('user.reset-password', ['token' => $token]);
+    })->name('password.reset');
+
+    // Handling The Form Submission
+    Route::post('/reset-password', function (Request $request) {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:4|confirmed',
+        ]);
+    
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+    
+                $user->save();
+    
+                event(new PasswordReset($user));
+            }
+        ); 
+    
+        return $status === Password::PASSWORD_RESET
+                    ? redirect()->route('login')->with(['success_msg' => __($status)])
+                    : back()->with(['failed_msg' => __($status)]);
+    })->name('password.update');
+});
+
+Route::middleware('auth')->group(function() {
+    // Logout
+    Route::get('/logout', [UserController::class, 'logout']);
+
+    // Email Verification Handler
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+        return redirect('/menu');
+    })->middleware(['auth', 'signed'])->name('verification.verify');
+
+    Route::middleware('verified')->group(function() {
+        // Show Dashboard
+        Route::get('/dashboard', [MealController::class, 'dashboard'])->middleware('role:admin');
+
+        // Add Meal
+        Route::post('/meal/store', [MealController::class, 'store']);
+
+        // Delete Meal
+        Route::delete('/delete/{meal}', [MealController::class, 'delete']);
+
+        // Edit Meal
+        Route::get('/edit/{meal}', [MealController::class, 'edit']);
+
+        // Update Meal
+        Route::put('/meal/update/{meal}', [MealController::class, 'update']);
+
+        // Account Settings
+        Route::get('/settings', [UserController::class, 'edit']);
+
+        // Update Account Info
+        Route::put('/account/update', [UserController::class, 'Update']);
+    }); 
+
+    Route::middleware('role:superAdmin')->group(function() {
+        // Super Admin Dashboard
+        Route::get('/super/dashboard', [UserController::class, 'dashboard']);
+
+        // Set Admin
+        Route::patch('/set-admin/{user}', [UserController::class, 'setAdmin']);
+    });
+});
 
 // Show the menu
 Route::get('/menu', [MealController::class, 'menu']);
 
-// Show Login Form
-Route::get('/login', [UserController::class, 'login'])->name('login');
-
-// Show Register Form
-Route::get('/register', [UserController::class, 'register']);
-
-// Submit Register Form
-Route::post('/users', [UserController::class, 'store']);
-
-// Submit Login Form
-Route::post('/auth', [UserController::class, 'authenticate']);
-
-// Logout
-Route::get('/logout', [UserController::class, 'logout']);
-
-// Show Dashboard
-Route::get('/dashboard', [MealController::class, 'dashboard'])->middleware(['auth', 'verified', 'role:admin']);
-
-// Add Meal
-Route::post('/meal/store', [MealController::class, 'store'])->middleware(['auth', 'verified']);
-
-// Delete Meal
-Route::delete('/delete/{meal}', [MealController::class, 'delete'])->middleware(['auth', 'verified']);
-
-// Edit Meal
-Route::get('/edit/{meal}', [MealController::class, 'edit'])->middleware(['auth', 'verified']);
-
-// Update Meal
-Route::put('/meal/update/{meal}', [MealController::class, 'update'])->middleware(['auth', 'verified']);
-
 // Show Single Meal
 Route::get('/meal/{meal}', [MealController::class, 'show']);
-
-// Account Settings
-Route::get('/settings', [UserController::class, 'edit'])->middleware(['auth', 'verified']);
-
-// Update Account Info
-Route::put('/account/update', [UserController::class, 'Update'])->middleware(['auth', 'verified']);
-
-// Super Admin Dashboard
-Route::get('/super/dashboard', [UserController::class, 'dashboard'])->middleware(['auth', 'role:superAdmin']);
-
-// Set Admin
-Route::patch('/set-admin/{user}', [UserController::class, 'setAdmin']);
 
 // Email Verification Notice
 Route::get('/email/verify', function () {
     return view('auth.verify-email');
 })->middleware('auth')->name('verification.notice');
-
-// Email Verification Handler
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-    return redirect('/menu');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-// The Password Reset Link Request Form
-Route::get('/forgot-password', function () {
-    return view('user.forgot-password');
-})->middleware('guest')->name('password.request');
-
-// Handling The Form Submission
-Route::post('/forgot-password', function (Request $request) {
-    $request->validate(['email' => 'required|email']);
- 
-    $status = Password::sendResetLink(
-        $request->only('email')
-    );
- 
-    return $status === Password::RESET_LINK_SENT
-                ? back()->with(['success_msg' => __($status)])
-                : back()->with(['failed_msg' => __($status)]);
-})->middleware('guest')->name('password.email');
-
-// The Password Reset Form
-Route::get('/reset-password/{token}', function ($token) {
-    return view('user.reset-password', ['token' => $token]);
-})->middleware('guest')->name('password.reset');
-
-// Handling The Form Submission
-Route::post('/reset-password', function (Request $request) {
-    $request->validate([
-        'token' => 'required',
-        'email' => 'required|email',
-        'password' => 'required|min:4|confirmed',
-    ]);
- 
-    $status = Password::reset(
-        $request->only('email', 'password', 'password_confirmation', 'token'),
-        function ($user, $password) {
-            $user->forceFill([
-                'password' => Hash::make($password)
-            ])->setRememberToken(Str::random(60));
- 
-            $user->save();
- 
-            event(new PasswordReset($user));
-        }
-    ); 
- 
-    return $status === Password::PASSWORD_RESET
-                ? redirect()->route('login')->with(['success_msg' => __($status)])
-                : back()->with(['failed_msg' => __($status)]);
-})->middleware('guest')->name('password.update');
